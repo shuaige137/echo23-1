7eeb9791f0190f180a869962f50b27b15a6a7cb6
Gendalf71 <gdedenko@gmail.com>
2024-10-07 21:01:58 +0300

    main

--- client.py
+++ client.py
@@ -1,16 +1,36 @@
-import socket
-from time import sleep
+import socket  # Импортируем модуль socket для работы с сетевыми соединениями
+from time import sleep  # Импортируем функцию sleep для возможной задержки (хотя она не используется в данном коде)
 
+# Запрашиваем у пользователя адрес сервера и номер порта и преобразуем последний в целое число
+host=input("host:")
+port = int(input("port:"))
+
+# Создаем новый сокет
 sock = socket.socket()
+
+# Устанавливаем режим блокирующего ввода/вывода (по умолчанию и так блокирующий, эта строка избыточна)
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
+# Устанавливаем соединение с сервером на localhost (127.0.0.1) и указанном порту
+sock.connect((host, port))
+print("Connected to server") # пункт 4.i выполнен
+
+# Запрашиваем у пользователя строку для отправки
+msg = input("Your string:")
+# Закомментированная альтернатива - отправка фиксированной строки "Hi!"
+#msg = "Hi!"
+
+# Отправляем сообщение серверу, предварительно закодировав его в байты
 sock.send(msg.encode())
+print("Message sent to server") # пункт 4.iii выполнен
 
+# Получаем ответ от сервера (не более 1024 байт)
 data = sock.recv(1024)
+print("Message received from server") # пункт 4.iv выполнен
 
+# Закрываем соединение с сервером
 sock.close()
+print("Connection closed to server") # пункт 4.ii выполнен
 
+# Выводим декодированный ответ от сервера
 print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,44 @@
-import socket
+import socket  # Импортируем модуль socket для работы с сетевыми соединениями
 
+# Запрашиваем у пользователя номер порта и преобразуем его в целое число
+port = int(input("port:"))
+
+# Создаем новый сокет
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+# Устанавливаем опцию сокета SO_REUSEADDR, чтобы можно было повторно использовать адрес
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Привязываем сокет к указанному порту на всех доступных интерфейсах (пустая строка означает любой доступный адрес)
+sock.bind(('', port))
+
+print("Server is starting") # пункт 2.i выполнен
+
+# Начинаем прослушивание входящих соединений (0 означает максимально возможное количество соединений)
+sock.listen(0)
+print("Port", port, "is listing") # пункт 2.ii выполнен
+
+# Принимаем входящее соединение. conn - новый сокет для общения с клиентом, addr - адрес клиента
+conn, addr = sock.accept()
+print("Client is accepted") # пункт 2.iii выполнен
+print("Client adress:", addr[0])  # Выводим IP-адрес клиента
+print("Client port:", addr[1])    # Выводим порт клиента
+
+msg = ''  # Инициализируем пустую строку для хранения полученного сообщения
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    # Получаем данные от клиента (не более 1024 байт за раз)
+    data = conn.recv(1024)
+    if not data:   # Если данных больше нет, выходим из цикла
+        print("All data is accepted") # пункт 2.iv выполнен
+        break
+    msg += data.decode()  # Декодируем полученные данные и добавляем к сообщению
+    # Отправляем обратно клиенту полученное сообщение в верхнем регистре
+    conn.send(msg.upper().encode())
+    print("Message to client is sent") # пункт 2.v выполнен
 
-print(msg)
+print(msg)  # Выводим полное полученное сообщение
 
-conn.close()
+conn.close()  # Закрываем соединение с клиентом
+print("Connection is closed. Client is off") # пункт 2.vi выполнен
+print("Server is off") # пункт 2.vii выполнен
